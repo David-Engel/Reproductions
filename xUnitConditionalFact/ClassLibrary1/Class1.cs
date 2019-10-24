3df01bc416a4f082c608585a2708f431b44a3974
dengel <dengel@magnitude.com>
2019-10-24 11:27:58 -0700

    Initial repro

---
+++ xUnitConditionalFact/ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static bool testFalse() => false;
+
+        [ConditionalFact(nameof(testFalse))]
+        public static void test()
+        {
+            Assert.True(true);
+        }
+    }
+}
